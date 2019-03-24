47e1d82dd28b9e4488c68d37507eefd8baedecef
0xvext <0xvext+github@gmail.com>
2019-03-24 15:29:48 -0400

    Added comments

--- python-a-day-15.py
+++ python-a-day-15.py
@@ -6,10 +6,15 @@
 
 import netifaces
 
+# Define func
 def getMyAddress():
+    # Create a variable to pull from ens33 interface
     myAddress = netifaces.ifaddresses('ens33')
+    # Loop through items in resulting list
     for i in myAddress:
+        # Loop through items in resulting dict
         for j in myAddress[i]:
+            # Print the 'addr' element of each dict
             print(j['addr'])
-
+# Call fun
 getMyAddress()
