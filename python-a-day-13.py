88b42e555cb801dabeaf80a437b1888f1cf918bf
0xvext <0xvext+github@gmail.com>
2019-03-26 23:20:37 -0400

    Modified comments

--- python-a-day-13.py
+++ python-a-day-13.py
@@ -4,13 +4,9 @@
 
 from datetime import date
 
-#Write your function here!
 def get_todays_date():
     return date.today()
 
-#If you want to test your code, you can do so by calling
-#your function below. However, this is no longer required
-#for grading.
 print(get_todays_date())
 print(str(str(get_todays_date()).replace("-","/")))
 
